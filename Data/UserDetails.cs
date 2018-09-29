c15f2abeb2d0774eecebbbaaeabafc6f7de3b404
AyodeleSunday <boluwah2012@gmail.com>
2018-09-29 08:10:43 +0100

    Working on intents handling

---
+++ Data/UserDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace alexa_challenge.Data
+{
+    public class UserDetails
+    {
+        public int Id { get; set; }
+        public string DeviceId { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
